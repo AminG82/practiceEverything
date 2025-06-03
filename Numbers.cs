b4bc29e68732e8d1cffb6a5c4b76286bad25c267
Amin Ghasemi <mmnghasemi@gmail.com>
2025-06-03 11:54:16 +0330

    Numbers.cs Added / params keyword Covered

---
+++ Numbers.cs
@@ -0,0 +1,14 @@
+﻿namespace nothing01
+{
+    public class Numbers
+    {
+        public static void printNumbers(params int[]numArray)
+        {
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine(numArray[i]);
+            }
+                
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@
         {
             Console.WriteLine(RecursiveFunction.recursiveFunc(5));    //RecursiveFunction
 
-            Animals human = new Animals();      //check the Animals Class to see understand Method OverLoading
+            Animals human = new Animals();      //check  Animals.cs to see Method OverLoading
             human.walk("1", "2");
 
             Animals sheep = new Animals();
             sheep.walk("1", "2" , "3","4");
+
+            Numbers.printNumbers(1, 4, 5, 2, 5, 2);     //check Numbers.cs to see params syntax
         }
     }
 }
